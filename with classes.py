50500cb70e97c0d1c82b4d0fddf899c5ead37758
BBorisov95 <72103381+BBorisov95@users.noreply.github.com>
2020-10-30 12:34:48 +0200

    Update with classes.py

--- with classes.py
+++ with classes.py
@@ -10,30 +10,23 @@ class Report():
         self.company_new_period = file.loc[file['Unnamed: 0'] == 'Цялостен резултат']
         self.company_new_period_value = self.company_new_period.iloc[:, 6].values
 
-class ReportOld():
-    def __init__(self, file):
-        super().__init__(file)
-        self.file = file
-    def old_period_turnover(self):
-        ot3_old_turnover = self.file.iloc[:, 2]
-        return ot3_old_turnover.values
+    class ReportOld():
+        def old_period_turnover(self):
+            ot3_old_turnover = self.file.iloc[:, 2]
+            return ot3_old_turnover.values
     
-    def old_period_sold_pcs(self):
-        ot3_old_sold_pcs = self.file.iloc[:, 11]
-        return ot3_old_sold_pcs.values
+        def old_period_sold_pcs(self):
+            ot3_old_sold_pcs = self.file.iloc[:, 11]
+            return ot3_old_sold_pcs.values
     
-class ReportNew():
-    def __init__(self, file):
-        super().__init__(file)
-        self.file = file
-        
-    def new_period_turnover(self):
-        ot3_new_turnover = self.file.iloc[:, 5]
-        return ot3_new_turnover.values
+    class ReportNew():
+        def new_period_turnover(self):
+            ot3_new_turnover = self.file.iloc[:, 5]
+            return ot3_new_turnover.values
     
-    def new_period_sold_pcs(self):
-        ot3_new_sold_pcs = self.file.iloc[:, 14]
-        return ot3_new_sold_pcs.values
+        def new_period_sold_pcs(self):
+            ot3_new_sold_pcs = self.file.iloc[:, 14]
+            return ot3_new_sold_pcs.values
     
 
         
@@ -43,9 +36,9 @@ excel_df = pd.read_excel(r'C:\Users\Bobi\Desktop\Сравнение по гру�
 ot3 = Report(excel_df)
 
 
-ot3_difference_in_percent = (((ot3.new_period_turnover() / ot3.old_period_turnover() - 1) * 100)
-ot3_difference_in_bgn = (ot3.new_period_turnover() - ot3.old_period_turnover())
-percent_from_company = ((ot3.new_period_turnover() / Report.self.company_new_period_value) * 100)
-sold_value_new = (ot3.new_period_turnover() / ot3.new_period_sold_pcs())
-sold_value_old = (ot3.old_period_turnover() / ot3.old_period_sold_pcs)
+ot3_difference_in_percent = ((ot3.ReportNew.new_period_turnover() / ot3.ReportOld.old_period_turnover() - 1) * 100)
+ot3_difference_in_bgn = (ot3.ReportNew.new_period_turnover() - ot3.ReportOld.old_period_turnover())
+percent_from_company = ((ot3.ReportNew.new_period_turnover() / Report.self.company_new_period_value) * 100)
+sold_value_new = (ot3.ReportNew.new_period_turnover() / ot3.ReportNew.new_period_sold_pcs())
+sold_value_old = (ot3.ReportOld.old_period_turnover() / ot3.ReportOld.old_period_sold_pcs)
  
